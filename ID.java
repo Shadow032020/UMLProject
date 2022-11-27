0aed79a058f52e62eea189b461038668642d4bf3
GaboNueve <94602569+GaboNueve@users.noreply.github.com>
2022-11-27 18:43:20 +0100

    Add files via upload

---
+++ ID.java
@@ -0,0 +1,11 @@
+public class ID {
+    private String typeID;
+    private String valueID;
+
+    public ID(String typeID, String valueID) {
+        this.valueID = valueID;
+        this.typeID = typeID;
+
+    }
+
+}
